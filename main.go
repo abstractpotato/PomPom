313481b76b2e79525e87af98a1844b37004e7212
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-03-21 18:07:08 -0400

    Mar 21, 2026 - 06:07 PM

--- main.go
+++ main.go
@@ -5,40 +5,67 @@ import (
   "net/http"
   "pom/modules"
   "log"
+  _ "embed"
 )
 
-const html = "./src/index.html"
-const css  = "./src/style.css"
-const js   = "./src/script.js"
-const fav  = "./src/favicon.ico"
-const logo = "./src/logo.svg"
-const ring = "./src/ring.wav"
+//go:embed src/index.html
+var html []byte
+
+//go:embed src/style.css
+var css []byte
+
+//go:embed src/script.js
+var js []byte
+
+//go:embed src/favicon.ico
+var fav []byte
+
+//go:embed src/logo.svg
+var logo []byte
+
+//go:embed src/ring.wav
+var ring []byte
 
 func main() {
-  port := ":8000"
+  port := ":6060"
+
   log.Printf("Starting Simple Webserver on port %s", port)
   router := http.NewServeMux()
-  router.HandleFunc("/", Display(html, "html"))
-  router.HandleFunc("/style.css", Display(css, "css"))
-  router.HandleFunc("/script.js", Display(js, "js"))
-  router.HandleFunc("/favicon.ico", Display(fav, "ico"))
-  router.HandleFunc("/logo.svg", Display(logo, "svg"))
-  router.HandleFunc("/ring.wav", Display(ring, "wav"))
+  router.HandleFunc("/", Show(html, "html"))
+  router.HandleFunc("/style.css", Show(css, "css"))
+  router.HandleFunc("/script.js", Show(js, "js"))
+  router.HandleFunc("/favicon.ico", Show(fav, "ico"))
+  router.HandleFunc("/logo.svg", Show(logo, "svg"))
+  router.HandleFunc("/ring.wav", Show(ring, "wav"))
+
   server := http.Server{ Addr: port, Handler: modules.Logging(router), }
   server.ListenAndServe()
 }
 
 func Display(file_name, file_type string) http.HandlerFunc {
   return func (w http.ResponseWriter, r *http.Request) {
+
     switch file_type {
       case "css": w.Header().Set("Content-Type", "text/css")
       case "js":  w.Header().Set("Content-Type", "text/javascript")
     }
+
     content, err := os.ReadFile(file_name)
     if err != nil {
       w.Write([]byte("Error 500"))
       return
     }
     w.Write(content)
+  }
+}
+
+func Show(file []byte, file_type string) http.HandlerFunc {
+  return func (w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "max-age=3600")
+    switch file_type {
+      case "css": w.Header().Set("Content-Type", "text/css")
+      case "js":  w.Header().Set("Content-Type", "text/javascript")
+    }
+    w.Write(file)
   }
 }
